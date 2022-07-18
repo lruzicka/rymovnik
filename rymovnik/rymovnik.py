266389ed811aa57c36f0dc4cdd13b4b4beadc8db
Lukas Ruzicka <lruzicka@redhat.com>
2022-07-18 17:58:36 +0200

    Add possibility to load external file.

--- rymovnik/rymovnik.py
+++ rymovnik/rymovnik.py
@@ -4,9 +4,13 @@
 Tento program je velmi jednoduchý vyhledávač výrazů ve slovníku
 českých slov, který lze například použít pro vyhledávání rýmů.
 
-Vytvořil Lukáš Růžička (luckyrose77@gmail.com), 2022.
+Vytvořil Lukáš Růžička (lruzicka@redhat.com), 2022.
 
-Program je vytvořen pod licencí GPL.
+Program je vytvořen pod licencí GPL. Pokud jste s programem
+neobdrželi licenci, najdete ji na adrese 
+https://www.gnu.org/licenses/gpl-3.0.html.
+
+Program se poskytuje bez jakýchkoliv záruk na jeho funkčnost.
 """
 
 import argparse
@@ -23,6 +27,7 @@ class Prikazy:
         self.radek.add_argument('-s', '--shoda', help="Najde shodu kdekoliv ve slově.")
         self.radek.add_argument('-d', '--delka', help="Omezí výsledky na slova s určenou délkou.")
         self.radek.add_argument('-r', '--regex', help="Vyhledá slova podle regulárního výrazu.")
+        self.radek.add_argument('-l', '--slovnik', help="Načte navíc slova ze slovníku zvoleného souboru.")
 
     def prikazy(self):
         """ Vrátí načtená vyhledávací kritéria. """
@@ -31,7 +36,7 @@ class Prikazy:
 
 class Slovnik:
     """ Reprezentuje slovník českých slov. """
-    def __init__(self):
+    def __init__(self, externi=None):
         """ Načte slovník z externího souboru a připraví jej jako seznam slov. """
         # Načti obsah přibaleného souboru s daty.
         soubor = pkgutil.get_data('rymovnik','slovnik.dic')
@@ -43,6 +48,13 @@ class Slovnik:
         # Projdi data a očisti je od bílých symbolů
         ocistena_data = [radek.strip() for radek in data]
         self.slovnik = ocistena_data
+        # Jestliže chceme externí data, tak se načtou
+        # zde:
+        if externi:
+            with open(externi, 'r') as extradata:
+                data = extradata.readlines()
+            data = [x.strip() for x in data]
+            self.slovnik = self.slovnik + data
         
     def najdi_shodu(self, vyraz):
         """ Najde všechny slova, která obsahují daný výraz. """
@@ -88,13 +100,20 @@ class Tiskarna:
 
 def main():
     """ Provádí hlavní tělo programu.  """
-    
+    # Vyřešíme příkazový řádek
     prikazovy_radek = Prikazy()
     vyber = prikazovy_radek.prikazy()
-
-    slova = Slovnik()
+    
+    # Podle toho, jestli si uživatel přeje externí data,
+    # vytvoříme datový objekt.
+    if vyber.slovnik:
+        slova = Slovnik(vyber.slovnik)
+    else:
+        slova = Slovnik()
+    # Načteme objekt tiskárny
     tisk = Tiskarna()
-
+    
+    # Podle příkazového řádku vybereme slova ze slovníku
     if vyber.konec:
         nalezeno = slova.najdi_konec(vyber.konec)
     elif vyber.shoda:
